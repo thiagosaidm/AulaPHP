0bd371539cfd95312aa4b0e6a57171a5c10d9e8a
thiagosaidm <thiag.said@gmail.com>
2023-05-06 13:04:45 -0300

    "primitivos"

---
+++ Aula02/ex002/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tipos primitivos de PHP</h1>
+    <h2>Testes de tipos primitivos</h2>
+    <?php 
+        $numFloat = 453.5;
+        $num = 300;
+        $off = true;
+        echo "o valor da variável é $num ";
+        var_dump($num);
+        echo " - $numFloat esse é um exemplo de: ";
+        var_dump($numFloat);
+        var_dump($off);
+    ?>
+</body>
+</html>
