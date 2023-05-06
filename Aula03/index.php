489bb3b9fbcef0632a0b78328af653d2616e960b
thiagosaidm <thiag.said@gmail.com>
2023-05-06 16:58:36 -0300

    "Manipulação"

--- Aula03/index.php
+++ Aula03/index.php
@@ -13,7 +13,7 @@
 
     <?php 
         $name = "Thiago";
-
+        const SOBRENOME = "Soares";;
         echo " Usando double quoted: Meu nome é $name. <br>" ;
         
         echo ' Usando single quoted: meu nome é $name';
@@ -21,6 +21,30 @@
         echo "<br> 
         <p>No uso de <b>double quoted</b>, existe a interpretação do conteúdo.<br> Já no da <b>single quoted</b>, o conteúdo é mostrado
         como foi escrito</p>";
+
+        echo "No caso de CONSTANTES, funciona de outra forma, elas sempre precisam ser usadas com CONCATENAÇÃO, pois
+        como não usam $ para declarar a variável, são interpretadas como strings";
+
+        echo "<br> Meu sobrenome é " . SOBRENOME;
+
+        echo "<p> Quando usamos funções, elas seguem esse mesmo comportamento da constante, exemplo</p>";
+
+        echo "Função data sem concatenação: Estamos no ano de date('Y') <br>";
+
+        echo "Função data concatenada: Estamos no ano de " . date('Y');
+
+        echo "<h4>Sequencia de Escape</h4>";
+
+        echo "<p>Quando queremos mostrar dentro de uma string as aspas, é usado a sequncia de escape, reprensetado por
+        barras e aspas simples, dessa forma:
+        </p>";
+
+        echo "$name \"thi \" " . SOBRENOME;
+     
+
+
+
+
 
     
     ?>
