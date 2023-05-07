20244b80fc785a85d03371e05dbb8702298cdbf7
thiagosaidm <thiag.said@gmail.com>
2023-05-07 10:27:25 -0300

    "Funções Matemáticas"

---
+++ Aula06/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Funções matemáticas do PHP 🐘</h1>
+        <?php 
+            echo "<h2> Segue algumas funções matemáticas no php</h2>
+            <p>
+            - abs( ) - transforma qualquer valor em absoluto (tira o sinal de - ou de +) <br>
+            - ceil( ) - arrendonda para cima <br>
+            - floor( ) - arrendonda para baixo <br>
+            - round( ) - arrendonda de acordo com o número <br>
+            - pow( ) - potência
+            </p>
+            "
+
+        ?>
+</body>
+</html>
