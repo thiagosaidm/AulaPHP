1db5ff7582e5be792df957b776af425a91533d1e
thiagosaidm <thiag.said@gmail.com>
2023-05-16 20:40:15 -0300

    "Desafio8php"

--- Desafio8/index.php
+++ Desafio8/index.php
@@ -7,8 +7,28 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
-    echo "oi"
-    ?>
+ <main>
+    <h1>Bem vindo!</h1>
+    <h2>Preencha seus dados para continuar!</h2>
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+    <label for="name">Seu nome</label>
+    <input type="text" name="name" id="name" maxlength="10">
+    <label for="age">Idade</label>
+    <input type="number" name="age" id="age"> 
+    <label>
+        <input type="radio" name="genero" value="masc">
+        Masculino
+    </label>
+    <label>
+        <input type="radio" name="genero" value="femi">
+        Feminino
+    </label>
+    <input type="submit" value="Enviar">
+    
+
+
+    </form>
+
+ </main>
 </body>
 </html>
