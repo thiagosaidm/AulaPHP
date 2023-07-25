a923b7d382d364ad9f84dbedc572a611bf87d3a2
thiagosaidm <thiag.said@gmail.com>
2023-07-25 20:08:38 -0300

    "Aula Array"

--- Aula10/index.php
+++ Aula10/index.php
@@ -15,6 +15,24 @@
         
         echo $teste;
 
+        //Array
+            #é possível criar arrays de 3 maneiras no Php
+
+            #dessa forma estou indexando os valores, com a chave para cada um
+
+            $carros = array(0=>"Fiat Uno", 2=>"Pajero TR4", 3=>"Fox");
+
+            #o indice será gerado automaticamente
+            $motos = array();
+
+            $motos[] = "Kawasaki Ninja";
+            $motos[] = "Hornet";
+            $motos[] = "Broz";
+
+            $personagens = ["Ichigo", "Goku","Naruto"];
+            print_r($personagens);
+
+
 
 
     
